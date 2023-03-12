000a5c0338eccfc8e59df09fa417baa711f33790
Ali Ayad <33245901+alihayad@users.noreply.github.com>
2023-03-12 01:18:50 +0100

    adding functions to reduce code repetition

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,30 +1,36 @@
 from flask import Flask, jsonify, session, request
 from flask_cors import CORS
 from captcha.image import ImageCaptcha
-import string,random,base64
+import string
+import random
+import base64
 
 app = Flask(__name__)
 app.secret_key = 'wehaveahulk'
 CORS(app, supports_credentials=True, origins=["http://127.0.0.1:3000"])
 
-@app.route('/api/captcha', methods = ['GET'])
-def generate_captcha(): 
-   image = ImageCaptcha(width = 300, height = 100)
-   word_1=str(''.join(random.choices(string.hexdigits + string.digits, k = 4)))
-   word_2=str(''.join(random.choices(string.hexdigits + string.digits, k = 4)))
-   captcha_text =word_1+' '+word_2
-   session['captcha']=captcha_text 
-   img_data = image.generate(captcha_text)
-   img_str = "data:image/png;base64," + base64.b64encode(img_data.getvalue()).decode()   
-   return (jsonify({'img':img_str}),200,{'Content-Type': 'application/json'})
- 
-@app.route('/api/check', methods = ['POST'])
+@app.route('/api/captcha', methods=['GET'])
+def generate_captcha():
+    image = ImageCaptcha(width=300, height=100)
+    captcha_text = generate_random_captcha_text()
+    session['captcha'] = captcha_text
+    img_data = image.generate(captcha_text)
+    img_str = get_base64_image_string(img_data)
+    return jsonify({'img': img_str})
+
+@app.route('/api/check', methods=['POST'])
 def check_captcha():
-   data=request.get_json()
-   if data['txt'].lower()==session["captcha"].lower():
-      return(jsonify({'catchaAnswer':True}),200,{'Content-Type': 'application/json'})
-   else:
-      return(jsonify({'catchaAnswer':False}),200,{'Content-Type': 'application/json'})
-   
+    data = request.get_json()
+    if data['txt'].lower() == session['captcha'].lower():
+        return jsonify({'captchaAnswer': True})
+    else:
+        return jsonify({'captchaAnswer': False})
+
+def generate_random_captcha_text():
+    return ' '.join(''.join(random.choices(string.hexdigits + string.digits, k=4)) for _ in range(2))
+
+def get_base64_image_string(img_data):
+    return "data:image/png;base64," + base64.b64encode(img_data.getvalue()).decode()
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
